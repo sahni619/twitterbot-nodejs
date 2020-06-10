12f5b3f268cf7de038d3e8f48c31c72d786a764a
sahni619 <sahni619@users.noreply.github.com>
2020-06-10 12:44:19 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Put your own Twitter App keys here. See README.md for more detail.
 module.exports = {
-  consumer_key: 'e9QHzDuyGT0yYn5a2LO3cUNwX',
-  consumer_secret: 'cYmz2m9ugjUTf5ZR5SGSedRzuOtuFskgMXuvEF8o5JiPhU3p2I',
-  access_token: '1259845456374444047-1lBwC3WY1l4mXZ4EQ9npbs01CyUB8Y',
-  access_token_secret: 'PhhiP4dE5uDyhQqYFmcoto6qsELrN050tN1rCGlOlOHXa'
+  consumer_key: 'blah',
+  consumer_secret: 'blah',
+  access_token: 'blah',
+  access_token_secret: 'blah'
 }
